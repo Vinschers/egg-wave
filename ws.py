cbea5d64c7b4b709cca1d7d621b63fcafbfa0086
PedroMReno <42344315+PedroMReno@users.noreply.github.com>
2020-04-19 16:24:31 -0300

    Teste do HMM e Teste com WS

--- ws.py
+++ ws.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>EGG WAVE</h1>"
+    return "<h1>EGG WAVE - FUNCIONANDO</h1>"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
