861685f7c633b44c4896a515f611213f01472c17
Scherer <fsvicentin@hotmail.com>
2020-05-31 18:46:32 -0300

    deploy hackathon #2

--- ws.py
+++ ws.py
@@ -3,8 +3,98 @@ from flask import Flask, jsonify, request, render_template, Response, send_file,
 import json
 import numpy as np
 import pandas as pd
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression
+
+#Vars de controle
+path = 'final.csv'
+drop = [0, 1, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23, 24, 25, 26, 27, 28, 30, 31]
+prever = [0, 1, -1] #Indices do novo DF!!
+
+#Vars globais
+normalizer = -1
+df = pd.DataFrame()
+lm = -1
+
+def inialize():
+    global df
+    global prever
+    global path
+    global normalizer
+    global drop
+    global lm
+
+    #Lendo
+    df = pd.read_csv(path)
+
+    #Arrumando colunas
+    df.drop(df.columns[drop], axis=1, inplace=True)
+
+    df_cases = df.iloc[:, 2]
+    df.drop('Cases_per_mil', axis=1, inplace=True)
+
+    cs = df.columns
+
+    #Normalizando
+    normalizer = preprocessing.Normalizer().fit(df)
+    df = normalizer.transform(df)
+    df = pd.DataFrame(df, columns = cs)
+
+    df['Cases_per_mil'] = df_cases
+
+    #Treinando
+    lm = LinearRegression()
+    lm.fit(df.iloc[2:, 2:-1], df.iloc[2:, 1])
+
+
+def classify(dd, hb, pa, up):
+    global df
+    global lm
+
+    df_test = normalizer.transform([[dd, hb, pa, up]])
+    df_test = pd.DataFrame(df_test)
+    
+    predict = lm.predict(df_test)
+    
+    mudanca = 0
+    menorDiferencaSegura = 9999
+    menorDiferenca = 9999
+    resulMaisProximo = 0
+    
+    for i in range(0, len(df.index)-1):
+       
+        dife = abs(df.iloc[i, 0] - predict[0])
+        
+        if df.iloc[i, -1] == 'Below' and dife < menorDiferencaSegura:
+            menorDiferencaSegura = dife
+            maiorSubDif = -1
+            
+            for ii in range(0, len(df.columns) - 3):
+                subDif = abs(df.iloc[i, ii + 2] - df_test.iloc[0, ii])
+                
+                if subDif > maiorSubDif:
+                    maiorSubDif = subDif
+                    
+                    if df.iloc[i, ii + 2] > df_test.iloc[0, ii]:
+                        mudanca = (ii + 1)
+                    else:
+                        mudanca = -(ii + 1)
+            
+        if dife < menorDiferenca:
+            menorDiferenca = dife
+            resulMaisProximo = df.iloc[i, -1]
+            
+    return [resulMaisProximo, mudanca]
+
+inialize()
+
 
 data = {}
 
@@ -37,17 +127,18 @@ def getData():
 
 
 
-@app.route("/submit", methods=['POST'])
+@application.route("/submit", methods=['POST'])
+@cross_origin()
 def submit():
     density = request.form['density']
-    air = request.form['air']
     icu = request.form['icu']
-    isolation = request.form['isolation']
+    elder = request.form['elder']
+    population = request.form['population']
 
-    # chamar algoritmo pedro
-    return "teste submit" # retorna json pro site 
+    return classify(density, icu, elder, population).to_json() # retorna json pro site 
     
-@app.route('/country/<name>', methods=['GET'])
+@application.route('/country/<name>', methods=['GET'])
+@cross_origin()
 def country(name):
     df = pd.read_csv('final.csv')
     df.set_index('Name', inplace=True)
